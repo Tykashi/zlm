2832cd903d50cc6317bf9cd85af418912ad5368e
Tykashi <me@tykashi.dev>
2025-05-09 04:01:17 +0000

    fix: added missing lib_mod imports

--- build.zig
+++ build.zig
@@ -68,7 +68,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     exe_mod.addImport("build", build_zig_zon);
-
+    lib_mod.addImport("build", build_zig_zon);
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
     // for actually invoking the compiler.
