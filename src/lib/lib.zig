805584dca764a50e1117e69eae0dcaacd9be7217
Tykashi <me@tykashi.dev>
2025-05-09 01:11:57 +0000

    fix: imports

--- src/lib/lib.zig
+++ src/lib/lib.zig
@@ -1,6 +1,3 @@
-const zcont = @import("zcont");
 pub const Manager = @import("manager.zig").Manager;
 pub const ZLMStartPlan = @import("manager.zig").ZLMStartPlan;
 pub const ManagerControlMessage = @import("manager.zig").ManagerControlMessage;
-
-pub const Context = zcont.Context;
